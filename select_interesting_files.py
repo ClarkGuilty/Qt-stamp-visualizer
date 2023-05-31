82514ae4183d1549e52fcb5a7a3322b048b196d2
Javier Acevedo <ja.acevedo12@uniandes.edu.co>
2023-05-31 10:59:27 +0200

    making the selecting_interesting_files script a bit more useful

--- select_interesting_files.py
+++ select_interesting_files.py
@@ -10,16 +10,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from os.path import join
+
+import argparse
 #%%
-classification_path = "Classifications"
-#classification_csv = "classification_autosave1_early_test.csv"
-classification_csv = "classification_mosaic_autosave__33107_99.csv"
-classification_csv = "classification_mosaic_autosave_false_positive_inspection_943_10_99.csv"
+parser = argparse.ArgumentParser(description='Configure the parameters of the execution.')
+parser.add_argument('-c',"--path_to_classification", help="Path to classification csv")
+args = parser.parse_args()
+#%%
+# classification_path = "Classifications"
+# classification_csv = "classification_autosave1_early_test.csv"
+# classification_csv = "classification_mosaic_autosave__33107_99.csv"
+# classification_csv = "classification_mosaic_autosave_false_positive_inspection_943_10_99.csv"
 
-df = pd.read_csv(join(classification_path,classification_csv))
+# df = pd.read_csv(join(classification_path,classification_csv))
+df = pd.read_csv(args.path_to_classification)
 
 
-selected_df = df[df.classification > 0]
+selected_df = df[df.classification == 1.0]
 for index, row in selected_df.iterrows():
     # print(row)
 # /home/alejandro/imported/inferences
